da7ad711c8bddd0ce1ad6f1f9e6a3cc0c410bee3
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-04 19:35:54 +0700

    commit15

--- chainfunctioncall.cpp
+++ chainfunctioncall.cpp
@@ -2,4 +2,14 @@
 using namespace std;
 class buku{
     string judul;
+
+public:
+    buku & setJudul(string judul){
+        this->judul = judul;
+        return *this;
+    }
+    string getJudul(){
+        return this->judul;
+    }
+
 };
