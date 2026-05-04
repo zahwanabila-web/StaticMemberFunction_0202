1e6f9de3e221a6dd23d8209f158da94581ac021b
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-04 19:30:25 +0700

    commit13

--- destructor.cpp
+++ destructor.cpp
@@ -26,3 +26,28 @@ angka::~angka(){
     cout <<"Alamat Array Sudah Dilepaskan" << endl;
 }
 
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " =  " << arr[i] << endl;
+    }
+}
+
+void angka::isiData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+    cout << endl;
+}
+int main()
+{
+    angka belajarcpp(3);
+    angka *ptrBelajarcpp = new angka(5);
+    delete ptrBelajarcpp;
+
+    return 0;
+}
