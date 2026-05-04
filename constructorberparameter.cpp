fab6facac4126d0ee08442324ea97335c4164f2c
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-04 19:24:36 +0700

    commit7

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -6,3 +6,14 @@ class mahasiswa{
 public:
     mahasiswa(int nim, string nama);
 };
+
+mahasiswa::mahasiswa(int nim, string nama){
+    cout << "Constructor  Dengan Parameter Terpanggil" << endl;
+    cout << "NIM : " << nim << endl;
+    cout << "Nama : " << nama << endl;
+}
+
+int main(){
+    mahasiswa mhs(12345, "Pascal");
+    return 0;
+}
