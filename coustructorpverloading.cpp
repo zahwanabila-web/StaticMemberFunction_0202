63e52495a7153a3b07bf287c0e3c67439590f068
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-04 19:25:49 +0700

    commit8

---
+++ coustructorpverloading.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa{
+    private:
+    int nim;
+    string nama;
+    public:
+    mahasiswa();
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int iNim, string iNama);
+    void cetak();
+};
+
