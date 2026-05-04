914b570162b1aad91762c668dde9e9a6c6b5fcb7
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-04 19:30:00 +0700

    commit12

--- destructor.cpp
+++ destructor.cpp
@@ -14,3 +14,15 @@ public:
     void isiData();
 };
 
+angka::angka(int i){
+    panjang = i;
+    arr = new int [i];
+    isiData();
+}
+angka::~angka(){
+    cout << endl;
+    cetakData();
+    delete [] arr;
+    cout <<"Alamat Array Sudah Dilepaskan" << endl;
+}
+
