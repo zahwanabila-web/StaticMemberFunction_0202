073d36136a7f96730509072d9f55de94586e0f5f
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-04 19:27:28 +0700

    commit10

--- coustructorpverloading.cpp
+++ coustructorpverloading.cpp
@@ -30,3 +30,20 @@ mahasiswa :: mahasiswa(int iNim, string iNama){
     nama = iNama;
 }
 
+void mahasiswa :: cetak(){
+
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
+int main(){
+    mahasiswa mhs1 (102030);
+    mahasiswa mhs2 ("Andi");
+    mahasiswa mhs3(010203, "Santi");
+
+    cout << "Mahasiswa 1:"; mhs1.cetak();
+    cout << "Mahasiswa 2:"; mhs2.cetak();
+    cout << "Mahasiswa 3:"; mhs3.cetak();
+
+    return 0;
+}
