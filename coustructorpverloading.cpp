a18a5add8c8dbc87f3be7183f0b0e4e90a848e5e
ZahwaNabila <zahwanabila434@gmail.com>
2026-05-04 19:26:58 +0700

    commit9

--- coustructorpverloading.cpp
+++ coustructorpverloading.cpp
@@ -14,3 +14,19 @@ class mahasiswa{
     void cetak();
 };
 
+mahasiswa :: mahasiswa(){
+}
+
+mahasiswa ::  mahasiswa(int iNim){
+    nim = iNim;
+}
+
+mahasiswa :: mahasiswa(string iNama){
+    nama = iNama;
+}
+
+mahasiswa :: mahasiswa(int iNim, string iNama){
+    nim = iNim;
+    nama = iNama;
+}
+
